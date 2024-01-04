0c355653432a2b315ddeff9a39f382d55f095eff
jhk742 <jhk742@nyu.edu>
2024-01-04 13:51:36 +0900

    implemented functionality to toggle daterange textFields and buttons for user-specified categories (total, income, expense) within the account_balance form

--- src/AccountBalanceForm.java
+++ src/AccountBalanceForm.java
@@ -1,16 +1,13 @@
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
+import java.awt.event.*;
 import java.math.BigDecimal;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.time.YearMonth;
-import java.util.ArrayList;
+import java.util.*;
 import java.util.List;
 
 public class AccountBalanceForm extends JDialog {
@@ -27,15 +24,51 @@ public class AccountBalanceForm extends JDialog {
     private JComboBox comboBoxViewAllSort;
     private JComboBox comboBoxIncomeFilterByDate;
     private JComboBox comboBoxExpensesFilterByDate;
+    private JButton expenseDateRangeConfirmBtn;
+    private JButton incomeDateRangeConfirmBtn;
+    private JButton totalDateRangeConfirmBtn;
+    private JPanel totalDateRangePanel;
+    private JTextField txtTotalStartDate;
+    private JTextField txtTotalEndDate;
+    private JTextField txtIncomeStartDate;
+    private JTextField txtIncomeEndDate;
+    private JTextField txtExpenseStartDate;
+    private JTextField txtExpenseEndDate;
+
+    Map<String, JButton> dateRangeButtonMap = new HashMap<String, JButton>() {{
+        put("totalDateRangeConfirmBtn", totalDateRangeConfirmBtn);
+        put("incomeDateRangeConfirmBtn", incomeDateRangeConfirmBtn);
+        put("expenseDateRangeConfirmBtn", expenseDateRangeConfirmBtn);
+    }};
+    ArrayList<JTextField> dateRangeTxtFields = new ArrayList<>(
+            Arrays.asList(
+                    txtTotalStartDate,
+                    txtTotalEndDate,
+                    txtIncomeStartDate,
+                    txtIncomeEndDate,
+                    txtExpenseStartDate,
+                    txtExpenseEndDate
+            )
+    );
+
+    Map<String, JTextField> dateRangeTxtFieldMap = new HashMap<String, JTextField>() {{
+        put("txtTotalStartDate", txtTotalStartDate);
+        put("txtTotalEndDate", txtTotalEndDate);
+        put("txtIncomeStartDate", txtIncomeStartDate);
+        put("txtIncomeEndDate", txtIncomeEndDate);
+        put("txtExpenseStartDate", txtExpenseStartDate);
+        put("txtExpenseEndDate", txtExpenseEndDate);
+    }};
 
     public AccountBalanceForm(JFrame parent, loggedUser loggedU) {
         super(parent);
         setTitle("Account Balance");
         setContentPane(accountBalanceForm);
-        setMinimumSize(new Dimension(1500, 800));
+        setMinimumSize(new Dimension(1500, 1000));
         setModal(true);
         setLocationRelativeTo(parent);
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+
 
         addWindowListener(new WindowAdapter() {
             @Override
@@ -49,6 +82,15 @@ public class AccountBalanceForm extends JDialog {
         btnViewAll.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                toggleSpecificDateRangesAndButtons(
+                    new HashMap<String, JTextField>() {{
+                        put("txtTotalStartDate", txtTotalStartDate);
+                        put("txtTotalEndDate", txtTotalEndDate);
+                    }},
+                    new HashMap<String, JButton>() {{
+                        put("totalDateRangeConfirmBtn", totalDateRangeConfirmBtn);
+                    }}
+                );
                 comboBoxViewAllSort.setSelectedIndex(0);
                 toggleComboBoxes(true, false, false);
                 populateTableAll(tableTransactionHistory, loggedU, "default");
@@ -57,6 +99,15 @@ public class AccountBalanceForm extends JDialog {
         btnIncome.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                toggleSpecificDateRangesAndButtons(
+                    new HashMap<String, JTextField>() {{
+                        put("txtIncomeStartDate", txtIncomeStartDate);
+                        put("txtIncomeEndDate", txtIncomeEndDate);
+                }},
+                    new HashMap<String, JButton>() {{
+                        put("incomeDateRangeConfirmBtn", incomeDateRangeConfirmBtn);
+                    }}
+                );
                 comboBoxIncomeFilterByDate.setSelectedIndex(0);
                 toggleComboBoxes(false, true, false);
                 populateTableIncome(tableTransactionHistory, loggedU, new optionAndDateFilterObject(false, 0, 0));
@@ -65,6 +116,15 @@ public class AccountBalanceForm extends JDialog {
         btnExpense.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                toggleSpecificDateRangesAndButtons(
+                    new HashMap<String, JTextField>() {{
+                        put("txtExpenseStartDate", txtExpenseStartDate);
+                        put("txtExpenseEndDate", txtExpenseEndDate);
+                }},
+                    new HashMap<String, JButton>() {{
+                        put("expenseDateRangeConfirmBtn", expenseDateRangeConfirmBtn);
+                    }}
+                );
                 comboBoxExpensesFilterByDate.setSelectedIndex(0);
                 toggleComboBoxes(false, false, true);
                 populateTableExpenses(tableTransactionHistory, loggedU, new optionAndDateFilterObject(false, 0, 0));
@@ -91,7 +151,6 @@ public class AccountBalanceForm extends JDialog {
             @Override
             public void actionPerformed(ActionEvent e) {
                 String date = String.valueOf(comboBoxIncomeFilterByDate.getSelectedItem());
-                System.out.println(date);
                 if (!date.equals("--- Select Year-Month ---")) {
                     YearMonth yearMonth = YearMonth.parse(date);
                     int year = yearMonth.getYear();
@@ -112,6 +171,91 @@ public class AccountBalanceForm extends JDialog {
                 }
             }
         });
+
+        txtTotalStartDate.addFocusListener(new FocusAdapter() {
+            String startDate = "Enter Start Date: yyyy-MM-dd";
+            @Override
+            public void focusGained(FocusEvent e) {
+                super.focusGained(e);
+                if (txtTotalStartDate.getText().equals(startDate)) {
+                    txtTotalStartDate.setText("");
+                    txtTotalStartDate.setForeground(Color.BLACK);
+                }
+            }
+
+            public void focusLost(FocusEvent e) {
+                if (txtTotalStartDate.getText().isEmpty()) {
+                    txtTotalStartDate.setText(startDate);
+                    txtTotalStartDate.setForeground(Color.GRAY);
+                }
+            }
+        });
+    }
+
+    private void toggleSpecificDateRangesAndButtons(Map<String, JTextField> dateTxtField, Map<String, JButton> dateBtn) {
+        String startDate = "Enter Start Date: yyyy-mm-dd";
+        String endDate = "Enter End Date: yyyy-mm-dd";
+        for (Map.Entry<String, JTextField> entry : dateRangeTxtFieldMap.entrySet()) {
+            String targetKey = entry.getKey();
+            JTextField targetValue = entry.getValue();
+
+            if (!dateTxtField.containsKey(targetKey)) {
+                targetValue.setEnabled(false);
+                if (targetKey.contains("Start")) {
+                    targetValue.setText(startDate);
+                    targetValue.setForeground(Color.GRAY);
+                } else if (entry.getKey().contains("End")) {
+                    targetValue.setText(endDate);
+                    targetValue.setForeground(Color.GRAY);
+                }
+            } else {
+                targetValue.setEnabled(true);
+            }
+        }
+        for (Map.Entry<String, JButton> entry : dateRangeButtonMap.entrySet()) {
+            String targetKey = entry.getKey();
+            JButton targetButton = entry.getValue();
+            if (!dateBtn.containsKey(targetKey)) {
+                targetButton.setEnabled(false);
+            } else {
+                targetButton.setEnabled(true);
+            }
+        }
+    }
+
+    private void dateRangeTxtFieldAddFocusListener(JTextField txtField, String startOrEnd) {
+        String startDate = "Enter Start Date: yyyy-mm-dd";
+        String endDate = "Enter End Date: yyyy-mm-dd";
+        txtField.setText(startOrEnd.equals("start") ? startDate : endDate);
+        txtField.setForeground(Color.GRAY);
+        txtField.addFocusListener(new FocusAdapter() {
+            @Override
+            public void focusGained(FocusEvent e) {
+                if (txtField.getText().equals(startDate) || txtField.getText().equals(endDate)) {
+                    txtField.setText("");
+                    txtField.setForeground(Color.BLACK);
+                }
+            }
+
+            public void focusLost(FocusEvent e) {
+                if (txtField.getText().isEmpty()) {
+                    txtField.setText(startOrEnd.equals("start") ? startDate : endDate);
+                    txtField.setForeground(Color.GRAY);
+                }
+            }
+        });
+    }
+
+    private void toggleDateRangePlaceHolder(/*ArrayList<JTextField> dateRanges*/) {
+//        ArrayList<JTextField> dateRangeTxtFields = dateRanges;
+        for (int i = 0; i < dateRangeTxtFields.size(); i++) {
+            JTextField targetTxtfield = dateRangeTxtFields.get(i);
+            if (i % 2 == 0) {
+                dateRangeTxtFieldAddFocusListener(targetTxtfield, "start");
+            } else {
+                dateRangeTxtFieldAddFocusListener(targetTxtfield, "end");
+            }
+        }
     }
     private void getLoggedUserTotalBalance(loggedUser loggedU) {
         try {
@@ -359,13 +503,6 @@ public class AccountBalanceForm extends JDialog {
 
     }
 
-    private void initializeViewAllFilterComboBox() {
-        comboBoxViewAllSort.removeAllItems();
-        comboBoxViewAllSort.addItem("--- Filter By ---");
-        comboBoxViewAllSort.addItem("Income");
-        comboBoxViewAllSort.addItem("Expenses");
-    }
-
     private void toggleComboBoxes(boolean viewAll, boolean income, boolean expenses) {
         comboBoxViewAllSort.setEnabled(viewAll);
         comboBoxIncomeFilterByDate.setEnabled(income);
@@ -387,6 +524,13 @@ public class AccountBalanceForm extends JDialog {
         comboBoxViewAllSort.setEnabled(false);
         comboBoxIncomeFilterByDate.setEnabled(false);
         comboBoxExpensesFilterByDate.setEnabled(false);
+        toggleDateRangePlaceHolder(/*dateRangeTxtFields*/);
+        for (JTextField dates : dateRangeTxtFields) {
+            dates.setEnabled(false);
+        }
+        totalDateRangeConfirmBtn.setEnabled(false);
+        incomeDateRangeConfirmBtn.setEnabled(false);
+        expenseDateRangeConfirmBtn.setEnabled(false);
     }
 }
 
