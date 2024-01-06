0270ecb423b3bf3596e079fc65b302937b672fc0
jhk742 <jhk742@nyu.edu>
2024-01-06 12:23:05 +0900

    added test directory

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,24 +7,5 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) {
         loginForm login = new loginForm(null);
-
-        HttpProvider httpProvider = new HttpProvider();
-
-//        try {
-//            JSONObject jsonObj = httpProvider.makeGetRequest("https://v6.exchangerate-api.com/v6/YOUR-API-KEY/latest/USD");
-//            // Check if the response is not empty before creating a JSONObject
-//            if (!response.isEmpty()) {
-//                JSONObject jsonObj = new JSONObject(response);
-//
-//                // Now you can access the data from the JSONObject
-//                String result = jsonObj.getString("result");
-//                System.out.println("Result: " + result);
-//
-//            } else {
-//                System.out.println("Empty response");
-//            }
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
     }
 }
