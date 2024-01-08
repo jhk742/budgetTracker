34b03e95dfc2268f2ac6b47b1e2be7ce2f9ebf4b
jhk742 <jhk742@nyu.edu>
2024-01-08 18:35:55 +0900

    test case written for connectionProvider

--- src/main/java/Connectors/ConnectionProvider.java
+++ src/main/java/Connectors/ConnectionProvider.java
@@ -21,7 +21,4 @@ public class ConnectionProvider {
             throw e;
         }
     }
-
-
-
 }
---
+++ src/test/java/connectionProviderTest.java
@@ -0,0 +1,37 @@
+import Connectors.ConnectionProvider;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.SQLException;
+
+public class connectionProviderTest {
+
+    private static Connection testConnection;
+
+    @AfterClass
+    public static void setUp() {
+        try {
+            if (testConnection != null && !testConnection.isClosed()) {
+                testConnection.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testGetCon() throws SQLException {
+        try {
+            testConnection = ConnectionProvider.getCon();
+            DatabaseMetaData metaData = testConnection.getMetaData();
+            assertNotNull("The connection object is not null.", testConnection);
+            assertNotEquals("Metadata (URL) received from the Connection object is not an empty string.", metaData.getURL());
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
