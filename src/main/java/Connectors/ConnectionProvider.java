23580422075492472b96b820fcf6813230f13fde
jhk742 <jhk742@nyu.edu>
2024-01-17 11:06:37 +0900

    now makes use of AWS-RDS instead of on-premise database

--- src/main/java/Connectors/ConnectionProvider.java
+++ src/main/java/Connectors/ConnectionProvider.java
@@ -7,15 +7,19 @@ import java.sql.SQLException;
 
 public class ConnectionProvider {
 
-    private static final String jdbcUrl = "jdbc:mysql://localhost:3306/budgettracker?useTimezone=true&serverTimezone=Asia/Seoul";
-    private static final String username = "root";
-    private static final String password = "Kjhyeong0219!";
+    private static final String local_jdbcUrl = "jdbc:mysql://localhost:3306/budgettracker?useTimezone=true&serverTimezone=Asia/Seoul";
+    private static final String local_username = "root";
+    private static final String local_password = "Kjhyeong0219!";
+
+    private static final String AWSRDS_jdbcUrl = "jdbc:mysql://budgettracker.cfuygiu08mdn.ap-northeast-2.rds.amazonaws.com:3306/budgettracker?useTimezone=true&serverTimezone=Asia/Seoul";
+    private static final String AWSRDS_jdbcUrl_username = "root";
+    private static final String AWSRDS_jdbcUrl_password = "Kjhyeong0219!";
 
     private static Connection connection;
 
     public static Connection getCon() throws SQLException {
         try {
-            connection = DriverManager.getConnection(jdbcUrl, username, password);
+            connection = DriverManager.getConnection(AWSRDS_jdbcUrl, AWSRDS_jdbcUrl_username, AWSRDS_jdbcUrl_password);
             return connection;
         } catch(SQLException e) {
             throw e;
